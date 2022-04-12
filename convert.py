20f671a6a9a7ab371cca1253e46358dcd6697b51
ntanh1999 <ntanh1999@gmail.com>
2022-04-12 19:53:58 +0700

    second chance

--- convert.py
+++ convert.py
@@ -5,20 +5,20 @@ csv_file = '/home/ted/ubuntu/code_test/observed_features.csv'
 reader = csv.reader(open(csv_file, 'r'), delimiter= ',')
 header = next(reader)
 
-dictionary = {}
+dictionary = {} # contain the feature count of each depth and sample
 
 for row in reader:
     sample = {}
     sample_id = row[0]
     for col_name, cell in zip(header,row):
-        if cell == '':
-            cell = 0
-        # print(col_name + ' ' + cell)
-        result = re.match(r'depth-(\d+)_iter-\d+', col_name) 
+        result = re.match(r'depth-(\d+)_iter-\d+', col_name) # extract the depth
         if result != None:
             depth = result.group(1)
             if depth in sample:
-                cell = int(float(cell))
+                if cell == '':
+                    cell = None
+                else:
+                    cell = int(float(cell)) # convert all value to integer because some are float
                 sample[depth].append(cell)
             else:
                 sample[depth] = []
@@ -29,12 +29,14 @@ for row in reader:
 outfile = '/home/ted/ubuntu/code_test/modifiled.csv'
 with open(outfile, 'w') as out_fh:
     writer = csv.writer(out_fh, delimiter=',')
+    writer.writerow(['sample_id', 'depth', 'feature_count']) # write the header
     for sample_id in dictionary:
         for depth in dictionary[sample_id]:
             feature_count_list = dictionary[sample_id][depth]
-            row = []
-            row.append(sample_id)
-            row.append(depth)
-            row.extend(feature_count_list)
+            for count in feature_count_list:
+                row = []
+                row.append(sample_id)
+                row.append(depth)
+                row.append(count)
 
-            writer.writerow(row)
+                writer.writerow(row)
--- plot.R
+++ plot.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 
-df <- read.csv('D:/ubuntu/code_test/modifiled.csv', sep = ',', header = FALSE)
+df <- read.csv('D:/ubuntu/code_test/modifiled.csv', sep = ',', header = TRUE)
 
-p <- ggplot(df, aes(x=V2, y =, fill=V1))
-p <- p + geom_boxplot() 
+p <- ggplot(df, aes(x=depth, y = feature_count, group=depth))
+p <- p + geom_boxplot() + facet_wrap(~sample_id)
 
 print(p)
